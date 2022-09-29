3c42677a2a7d43720bab4c52fce388a4ee1b1237
kirinji666 <kirinji666@naver.com>
2022-09-29 17:19:07 +0900

    initialize

---
+++ java_0929/src/java_0929/apple.java
@@ -0,0 +1,27 @@
+package java_0929;
+
+public class apple {
+
+	public static void main (String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("히히");
+System.out.println("히히");
+System.out.println(2-1);
+System.out.print("히히");
+System.out.println("히히");
+System.out.println("2"+3);
+System.out.println("3+5=8");
+
+System.out.printf("%5d\n",20);
+System.out.printf("%56d\n",200);
+System.out.printf("%f\n",20.1223);
+System.out.printf("%.2f\n",20.2354);
+
+
+
+
+
+
+	}
+
+}
